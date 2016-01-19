8cb8191b0e26a8544c2d39c951ce89b874938dbf
carno <spamer2000@list.ru>
2016-01-19 13:05:26 +0500

    refs

---
+++ upload.php
@@ -0,0 +1,27 @@
+<?php
+require 'vendor/autoload.php';
+$options = getopt("vk:a:u:f:");
+if (empty($options["k"])) {
+	throw new \carono\appsun\Exception('Need set software key');
+}
+$appsun = new \carono\appsun\Appsun();
+$appsun->system_name = $options["k"];
+$appsun->version = $appsun->getNextVersion()->value;
+if (isset($options["v"])) {
+	exit($appsun->version);
+}
+if (isset($options["u"])) {
+	if (empty($options["a"])) {
+		throw new \carono\appsun\Exception('Need set upload key');
+	}
+	$appsun->api = $options["a"];
+	$data = require $options["f"];
+	if (isset($data["installer"])) {
+		foreach ($data["installer"] as $file => $slug) {
+			$appsun->uploadInstaller($file, $slug);
+		}
+	}
+	if (isset($data["files"])) {
+		$appsun->uploadFiles($data["files"]);
+	}
+}
